8abac6daacda05e6ca4c83adad6024389d571c46
Kreshnik Beqi <kreshnik@beqi.co>
2025-08-08 14:14:38 -0700

    Create layout.tsx

---
+++ src/app/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+// Update the default metadata for the entire application.
+export const metadata: Metadata = {
+  title: "LLMs.txt Generator",
+  description:
+    "Easily generate an LLMs.txt file to control which large language model crawlers may access your website.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
